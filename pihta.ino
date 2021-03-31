53572b5db56b64ceed90cb89416fe2792c37d602
Ramiolgam <81672743+Ramiolgam@users.noreply.github.com>
2021-03-31 09:57:42 +0300

    Update pihta.ino

--- pihta.ino
+++ pihta.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <angle_v3.h>
 
 String cCode;
 
@@ -6,10 +7,11 @@ Servo servoA;
 Servo servoB;
 Servo servoC;
 
-jekaCode polibomu;
+jekaCode kyoka;
 
 void setup() {
 
+Serial.begin(9600);
 
 servoA.attach(4);
 servoB.attach(3);
@@ -21,7 +23,7 @@ void loop() {
 
 if (Serial.available()>0){
   cCode = Serial.read();
-  set_gCode(cCode);
-  }
+  kyoka.set_gCode(cCode);
+}
   normalno();
 }
